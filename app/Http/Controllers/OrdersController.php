5bc3636e91991a2c3242f985f42f69e8c18a2f00
UmairRathore <umairrathore05@gmail.com>
2024-11-18 13:07:53 +0500

    OrderBotSettings saved

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -23,7 +23,26 @@ class OrdersController extends Controller
 
     public function storeOrderBotSetting(Request $request)
     {
-            dd($request);
+//            dd($request);
+
+        $settings = OrderBotSettings::create([
+            'order_range_min' =>$request->order_range_min  ?? '',
+            'order_range_max' => $request->order_range_max  ?? '',
+            'order_value_min' => $request->order_value_min  ?? '',
+            'order_value_max' => $request->order_value_max  ?? '',
+            'items_per_order_min' => $request->items_per_order_max  ?? '',
+            'items_per_order_max' => $request->items_per_order_min  ?? '',
+            'one_item_order_chance_min' => $request->one_item_chance_max  ?? '',
+            'one_item_order_chance_max' => $request->one_item_chance_min  ?? '',
+            'order_speed_min' => $request->order_speed_min  ?? '',
+            'order_speed_max' => $request->order_speed_max  ?? '',
+            'order_speed_unit' => $request->order_frequency  ?? '',
+            'telegramBot' => $request->telegramBot  ?? '',
+            'unlimitedOrders' => $request->unlimitedOrders  ?? '',
+            'csv_file_path' => $request->file_location ?? '',
+        ]);
+
+        dd($settings);
     }
 
 
